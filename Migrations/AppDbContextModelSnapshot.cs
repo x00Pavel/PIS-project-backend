28f7bea3fbb9ff6a269cf26c5bd20b54809b00dd
Pavel Yadlouski <pavel.yadlouski@veeam.com>
2023-03-23 19:03:39 +0100

    Add migrations

--- Migrations/AppDbContextModelSnapshot.cs
+++ Migrations/AppDbContextModelSnapshot.cs
@@ -19,7 +19,112 @@ namespace video_pujcovna_back.Migrations
                 .HasAnnotation("ProductVersion", "7.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-            modelBuilder.Entity("video_pujcovna_back.Models.RecordModel", b =>
+            modelBuilder.Entity("video_pujcovna_back.Models.ReservationModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("char(36)");
+
+                    b.Property<DateTime>("ReservationDate")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<DateTime>("ReturnDate")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<Guid>("UserId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<Guid>("VideotapeId")
+                        .HasColumnType("char(36)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UserId");
+
+                    b.HasIndex("VideotapeId");
+
+                    b.ToTable("Reservations");
+                });
+
+            modelBuilder.Entity("video_pujcovna_back.Models.RoleModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("char(36)");
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("longtext");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("varchar(255)");
+
+                    b.Property<int>("Priority")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Name");
+
+                    b.ToTable("Roles");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = new Guid("659195a5-3667-4350-b4c4-550fa8f1908e"),
+                            Description = "System administrator",
+                            Name = "admin",
+                            Priority = 0
+                        },
+                        new
+                        {
+                            Id = new Guid("5fc03444-56b7-4ca7-a7de-dd4bec93e44e"),
+                            Description = "Branch lead",
+                            Name = "lead",
+                            Priority = 20
+                        },
+                        new
+                        {
+                            Id = new Guid("c8ef7cfe-631d-4fdf-8705-9514a78d7f4e"),
+                            Description = "Regular employee",
+                            Name = "employee",
+                            Priority = 40
+                        },
+                        new
+                        {
+                            Id = new Guid("da055781-5fd2-47d7-86a1-84b2c5ddba08"),
+                            Description = "Regular customer",
+                            Name = "customer",
+                            Priority = 60
+                        });
+                });
+
+            modelBuilder.Entity("video_pujcovna_back.Models.UserModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("char(36)");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
+                        .HasColumnType("longtext");
+
+                    b.Property<Guid>("RoleId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<string>("Username")
+                        .IsRequired()
+                        .HasColumnType("longtext");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("video_pujcovna_back.Models.VideotapeModel", b =>
                 {
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
@@ -39,72 +144,37 @@ namespace video_pujcovna_back.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("RecordModel");
+                    b.ToTable("VideTape");
                 });
 
             modelBuilder.Entity("video_pujcovna_back.Models.ReservationModel", b =>
                 {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("char(36)");
-
-                    b.Property<Guid>("RecordId")
-                        .HasColumnType("char(36)");
-
-                    b.Property<DateTime>("ReservationDate")
-                        .HasColumnType("datetime(6)");
-
-                    b.Property<DateTime>("ReturnDate")
-                        .HasColumnType("datetime(6)");
-
-                    b.Property<Guid>("UserId")
-                        .HasColumnType("char(36)");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("RecordId");
-
-                    b.HasIndex("UserId");
-
-                    b.ToTable("reservations");
-                });
-
-            modelBuilder.Entity("video_pujcovna_back.Models.UserModel", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("char(36)");
-
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("longtext");
-
-                    b.Property<string>("Username")
-                        .IsRequired()
-                        .HasColumnType("longtext");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Users");
-                });
-
-            modelBuilder.Entity("video_pujcovna_back.Models.ReservationModel", b =>
-                {
-                    b.HasOne("video_pujcovna_back.Models.RecordModel", "Record")
-                        .WithMany()
-                        .HasForeignKey("RecordId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
                     b.HasOne("video_pujcovna_back.Models.UserModel", "User")
                         .WithMany("Reservations")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.Navigation("Record");
+                    b.HasOne("video_pujcovna_back.Models.VideotapeModel", "Videotape")
+                        .WithMany()
+                        .HasForeignKey("VideotapeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.Navigation("User");
+
+                    b.Navigation("Videotape");
+                });
+
+            modelBuilder.Entity("video_pujcovna_back.Models.UserModel", b =>
+                {
+                    b.HasOne("video_pujcovna_back.Models.RoleModel", "Role")
+                        .WithMany()
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Role");
                 });
 
             modelBuilder.Entity("video_pujcovna_back.Models.UserModel", b =>
